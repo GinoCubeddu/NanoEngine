195acf90f5ef1a9e3142c805d5876a29caa68788
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-01-30 18:37:20 +0000

    Rework AiFactory and AssetFactory
    This commit removes all but one method from each
    of the classes turning them into a proper factory.
    
    They now take IAsset instead of an IEntity, this means
    that the methods from IEntity have been moved to IAsset
    as they will be needed for ALL assets.

--- NanoEngine/ObjectManagement/Interfaces/IAssetFactory.cs
+++ NanoEngine/ObjectManagement/Interfaces/IAssetFactory.cs
@@ -3,64 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace NanoEngine.ObjectManagement.Interfaces
 {
     public interface IAssetFactory
     {
         /// <summary>
-        /// Using generics to create a new IEntity
-        /// </summary>
-        /// <typeparam name="T">Type of IEntity passed in</typeparam>
-        /// <param name="pos1">Position for x axis</param>
-        /// <param name="pos2">Position for y axis</param>
-        void AddEntity<T>(float pos1, float pos2, string uName) where T : IEntity, new();
-
-        /// <summary>
-        /// Passes back a list of the current Entitys
-        /// </summary>
-        /// <returns>List of type IEntity</returns>
-        List<IEntity> GetList();
-
-        /// <summary>
-        /// Emptys the current entity list
-        /// </summary>
-        void emptyList();
-
-        /// <summary>
-        /// Removes an entity from the list based on its ID
-        /// </summary>
-        /// <param name="id">Unique ID of an entity</param>
-        void remove(int id);
-
-        /// <summary>
-        /// Returns the entity by the ID passed in
-        /// </summary>
-        /// <param name="id">Id for the desired entity</param>
-        /// <returns>Entity qith the desired id</returns>
-        IEntity getEntity(int id);
-
-        /// <summary>
-        /// Add a list of entitys to the game
-        /// </summary>
-        /// <param name="list"></param>
-        void addEntitys(List<IEntity> list);
-
-        /// <summary>
-        /// Returns the entity by the name passed in
-        /// </summary>
-        /// <param name="name">name for the desired entity</param>
-        /// <returns>Entity qith the desired id</returns>
-        IEntity getEntityByName(string name);
-
-        /// <summary>
         /// Returns a new entity that can be used outside of the entity list
         /// </summary>
         /// <typeparam name="T">Type of entity</typeparam>
-        /// <param name="pos1">position on x axis</param>
-        /// <param name="pos2">position on y axis</param>
         /// <param name="uName">The name to give the entity</param>
+        /// <param name="position">Vector2 position of the asset</param>
         /// <returns>a newly created entity</returns>
-        IEntity RetriveNewEntity<T>(float pos1, float pos2, string uName) where T : IEntity, new();
+        IAsset RetriveNewAsset<T>(string uName, Vector2 position)
+            where T : IAsset, new();
     }
 }
--- NanoEngine/ObjectManagement/Managers/AiFactory.cs
+++ NanoEngine/ObjectManagement/Managers/AiFactory.cs
@@ -10,91 +10,23 @@ namespace NanoEngine.ObjectManagement.Managers
 {
     public class AiFactory : IAiFactory
     {
-        //Static field to hold an instance to the manager
-        private static IAiFactory manager;
-
-        ///List to hold all the current AI's
-        private List<IAIComponent> _AIs;
-
-        ///getter for the instance of the manager
-        public static IAiFactory Manager
-        {
-            get { return manager ?? (manager = new AiFactory()); }
-        }
-
-        /// <summary>
-        /// Private constructor for the AIManager
-        /// </summary>
-        private AiFactory()
-        {
-            _AIs = new List<IAIComponent>();
-        }
-
-        /// <summary>
-        /// Method to update all behaviours
-        /// </summary>
-        public void Update()
-        {
-            foreach (IAIComponent item in _AIs)
-            {
-                item.Update();
-            }
-        }
-
         /// <summary>
         /// Factory method to create an AI
         /// </summary>
         /// <typeparam name="T">Type of AI wanted</typeparam>
-        /// <param name="ent"></param>
+        /// <param name="asset"></param>
         /// <returns>The created AI</returns>
-        public IAIComponent CreateAI<T>(IEntity ent) where T : IAIComponent, new()
+        public IAIComponent CreateAI<T>(IAsset asset) where T : IAIComponent, new()
         {
             //Create new AI
             IAIComponent AI = new T();
             //Init the ai
-            AI.Initialise(ent);
+            AI.Initialise(asset);
             //Check if its certian things
             EventManager.Manager.AddDelegates(AI);
             //Add AI to list
-            _AIs.Add(AI);
             //Return AI
             return AI;
-        }
-
-        /// <summary>
-        /// Emptys the current AI list
-        /// </summary>
-        public void EmptyList()
-        {
-            _AIs.Clear();
-            _AIs.TrimExcess();
-        }
-
-        /// <summary>
-        /// Returns the behaviour by the ID passed in
-        /// </summary>
-        /// <param name="id">Id for the desired entity</param>
-        /// <returns>Entity qith the desired id</returns>
-        public IAIComponent getAI(int id)
-        {
-            //Checks if there is an entity in the list that has the passed in ID
-            var AI = _AIs.Where(x => x.UID == id).ToArray();
-            return AI[0];
-        }
-
-        /// <summary>
-        /// Removes an entity from the list based on its ID
-        /// </summary>
-        /// <param name="id">Unique ID of an entity</param>
-        public void RemoveAI(int id)
-        {
-            //Checks if there is an entity in the list that has the passed in ID
-            var AI = _AIs.Where(x => x.UID == id).ToArray();
-            //Remove entity
-            _AIs.Remove(AI[0]);
-            EventManager.Manager.RemoveDelegates(AI[0]);
-            //Set the entity to null
-            AI[0] = null;
         }
     }
 }

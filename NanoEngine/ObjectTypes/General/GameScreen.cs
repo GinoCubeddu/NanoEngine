ea5694be2961f78cde5317640763dac89bb6a50d
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-01-30 18:54:51 +0000

    Update GameScreen classes to implement AssetManager

--- NanoEngine/ObjectTypes/General/GameScreen.cs
+++ NanoEngine/ObjectTypes/General/GameScreen.cs
@@ -1,5 +1,6 @@
 ﻿using NanoEngine.Core.Interfaces;
 using NanoEngine.Core.Managers;
+using NanoEngine.ObjectManagement.Interfaces;
 using NanoEngine.ObjectManagement.Managers;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,8 @@ namespace NanoEngine.ObjectTypes.General
 {
     public abstract class GameScreen : IGameScreen
     {
+        protected IAssetManager _assetManager = new AssetManager();
+
         /// <summary>
         /// Abstract method to force sub classes to implement it. It is used to load content to the screen 
         /// </summary>
@@ -21,19 +24,36 @@ namespace NanoEngine.ObjectTypes.General
         public virtual void UnloadContent()
         {
             ContentManagerLoad.Manager.UnloadAll();
-            AssetFactory.Manager.emptyList();
-            AiFactory.Manager.EmptyList();
         }
 
         /// <summary>
         /// Abstract method to force sub classes to implement it. It is used to update the screen 
         /// </summary>
-        public abstract void Update();
+        protected abstract void Update();
 
         /// <summary>
         /// Abstarct method to force sub classes to implement it
         /// </summary>
         /// <param name="renderManager">An instance of the render manager which will be used to draw</param>
-        public abstract void Draw(IRenderManager renderManager);
+        protected abstract void Draw(IRenderManager renderManager);
+
+        /// <summary>
+        /// Method to Draw the scene
+        /// </summary>
+        /// <param name="renderManager">RenderManager containing draw methods</param>
+        public void DrawScreen(IRenderManager renderManager)
+        {
+            _assetManager.DrawAssets(renderManager);
+            Draw(renderManager);
+        }
+
+        /// <summary>
+        /// Method to update the GameScreen
+        /// </summary>
+        public void UpdateScreen()
+        {
+            _assetManager.UpdateAssets();
+            Update();
+        }
     }
 }

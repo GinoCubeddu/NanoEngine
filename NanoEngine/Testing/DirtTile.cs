3a46b16b13b626de406dcdfa2be20fee85b1c6b2
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-01-30 19:03:06 +0000

    Add files that have been used for testing new layout

---
+++ NanoEngine/Testing/DirtTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using NanoEngine.Core.Managers;
+using NanoEngine.ObjectTypes.Assets;
+
+namespace NanoEngine
+{
+    class DirtTile : Tile
+    {
+        public DirtTile()
+        {
+            Texture = ContentManagerLoad.Manager.LoadResource<Texture2D>("Dirt_Tile");
+        }
+    }
+}

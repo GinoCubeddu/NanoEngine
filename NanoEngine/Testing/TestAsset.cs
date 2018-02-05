c879a4636c3aae34beeb783b5a5f6e53fc59c205
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2018-02-05 15:00:35 +0000

    Import code from the animation branch

--- NanoEngine/Testing/TestAsset.cs
+++ NanoEngine/Testing/TestAsset.cs
@@ -12,7 +12,37 @@ namespace NanoEngine
     {
         public override void Initilise()
         {
-            SetTexture(ContentManagerLoad.Manager.LoadResource<Texture2D>("player"));
+            // SetTexture(ContentManagerLoad.Manager.LoadResource<Texture2D>("player"));
+            IDictionary<string, IDictionary<string, int>> AnimationDict =
+                new Dictionary<string, IDictionary<string, int>>();
+
+            AnimationDict.Add("idleRight", new Dictionary<string, int>());
+            AnimationDict["idleRight"].Add("TextureWidth", 27);
+            AnimationDict["idleRight"].Add("TextureHeight", 64);
+            AnimationDict["idleRight"].Add("Row", 0);
+            AnimationDict["idleRight"].Add("ColumnCount", 8);
+
+            AnimationDict.Add("idleLeft", new Dictionary<string, int>());
+            AnimationDict["idleLeft"].Add("TextureWidth", 27);
+            AnimationDict["idleLeft"].Add("TextureHeight", 64);
+            AnimationDict["idleLeft"].Add("Row", 2);
+            AnimationDict["idleLeft"].Add("ColumnCount", 7);
+
+            AnimationDict.Add("runRight", new Dictionary<string, int>());
+            AnimationDict["runRight"].Add("TextureWidth", 35);
+            AnimationDict["runRight"].Add("TextureHeight", 64);
+            AnimationDict["runRight"].Add("Row", 1);
+            AnimationDict["runRight"].Add("ColumnCount", 6);
+
+            AnimationDict.Add("runLeft", new Dictionary<string, int>());
+            AnimationDict["runLeft"].Add("TextureWidth", 35);
+            AnimationDict["runLeft"].Add("TextureHeight", 64);
+            AnimationDict["runLeft"].Add("Row", 3);
+            AnimationDict["runLeft"].Add("ColumnCount", 6);
+
+            AssetAnimation = new Animation(
+                AnimationDict, ContentManagerLoad.Manager.GetTexture("player")
+            );
         }
     }
 }

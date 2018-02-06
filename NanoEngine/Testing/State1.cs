b65f509536eddd78eef13f65421246e4e5e6fcdd
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-02-06 22:52:39 +0000

    Create test states and test them in TestMind

---
+++ NanoEngine/Testing/State1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NanoEngine.Testing
+{
+    public class State1 : IState
+    {
+        /// <summary>
+        /// Method that gets called at the begining of each state
+        /// </summary>
+        /// <typeparam name="T">The type of AI that the state uses</typeparam>
+        /// <param name="owner">The AI that owns the state</param>
+        public void Enter<T>(T owner)
+        {
+            Console.WriteLine("Entering state 1");
+        }
+
+        /// <summary>
+        /// Method that gets called at the end of each state
+        /// </summary>
+        /// <typeparam name="T">The type of AI that the state uses</typeparam>
+        /// <param name="owner">The AI that owns the state</param>
+        public void Exit<T>(T owner)
+        {
+            Console.WriteLine("Exiting state 1");
+        }
+
+        /// <summary>
+        /// Method that updates the state
+        /// </summary>
+        /// <typeparam name="T">The type of AI that the state uses</typeparam>
+        /// <param name="owner">The AI that owns the state</param>
+        public void Update<T>(T owner)
+        {
+            Console.WriteLine("updating state 1");
+        }
+    }
+}

1199a62b45eefa954e44c324bd625f3db3c05940
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-02-06 21:55:59 +0000

    Create transitions and a holder

---
+++ NanoEngine/StateManagement/Transitions/KeyboardStateTransition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using NanoEngine.Events.Args;
+using NanoEngine.StateManagement.States;
+
+namespace NanoEngine.StateManagement.Transitions
+{
+    internal class KeyboardStateTransition
+    {
+        // Holds the type of which state to transition to
+        public Type StateTo { get; }
+
+        // Holds which key state we want to check for
+        private KeyStates _keyboardTransition;
+
+        // Holds all the keys required be be in the keystate for transistion to be true
+        private IList<Keys> _keys;
+
+        /// <summary>
+        /// Constructor for the keyboard transition
+        /// </summary>
+        /// <param name="type">The key state we are checking the keys against</param>
+        /// <param name="keysRequired">The keys required to be in the key state</param>
+        /// <param name="stateTo">The statebehaviour we want to transition to</param>
+        public KeyboardStateTransition(Type stateTo, KeyStates type, IList<Keys> keysRequired)
+        {
+            _keyboardTransition = type;
+            _keys = keysRequired;
+            StateTo = stateTo;
+        }
+
+        /// <summary>
+        /// Method that checks if the transition is valid
+        /// </summary>
+        /// <param name="eventArgs">event args holding the nessecery incofmation</param>
+        /// <returns>Boolean telling us if it is a valid transition</returns>
+        public bool CheckTransition(NanoKeyboardEventArgs eventArgs)
+        {
+            return _keys.All(key => eventArgs.TheKeys[_keyboardTransition].Contains(key));
+        }
+    }
+}
---
+++ NanoEngine/StateManagement/Transitions/MethodSTateTransition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NanoEngine.StateManagement.Transitions
+{
+    internal class MethodStateTransition
+    {
+        // Holds the type of which state to transition to
+        public Type StateTo { get; }
+
+        // Holds the method that we need to call for transition check
+        private Func<bool> _methodToCall;
+
+        /// <summary>
+        /// Constructor for the keyboard transition
+        /// </summary>
+        /// <param name="stateTo">The statebehaviour we want to transition to</param>
+        public MethodStateTransition(Type stateTo, Func<bool> methodToCall)
+        {
+            StateTo = stateTo;
+            _methodToCall = methodToCall;
+        }
+
+        /// <summary>
+        /// Method that checks if the transition is valid
+        /// </summary>
+        /// <returns>Boolean telling us if it is a valid transition</returns>
+        public bool CheckTransition()
+        {
+            return _methodToCall.Invoke();
+        }
+    }
+}

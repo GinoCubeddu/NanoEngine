d005963ef6f1c840b353576b5d2dd376fe0eed94
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-02-06 22:52:39 +0000

    Create test states and test them in TestMind

---
+++ NanoEngine/Testing/State3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NanoEngine.Testing
+{
+    class State3 : IState
+    {
+        /// <summary>
+        /// Method that gets called at the begining of each state
+        /// </summary>
+        /// <typeparam name="T">The type of AI that the state uses</typeparam>
+        /// <param name="owner">The AI that owns the state</param>
+        public void Enter<T>(T owner)
+        {
+            (owner as TestMind).Timer = 0;
+            Console.WriteLine("Entering state 3");
+        }
+
+        /// <summary>
+        /// Method that gets called at the end of each state
+        /// </summary>
+        /// <typeparam name="T">The type of AI that the state uses</typeparam>
+        /// <param name="owner">The AI that owns the state</param>
+        public void Exit<T>(T owner)
+        {
+            Console.WriteLine("Exiting state 3");
+        }
+
+        /// <summary>
+        /// Method that updates the state
+        /// </summary>
+        /// <typeparam name="T">The type of AI that the state uses</typeparam>
+        /// <param name="owner">The AI that owns the state</param>
+        public void Update<T>(T owner)
+        {
+            Console.WriteLine("Updating state 3");
+        }
+    }
+}
--- NanoEngine/Testing/TestMind.cs
+++ NanoEngine/Testing/TestMind.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using NanoEngine.Events.Args;
+using NanoEngine.ObjectTypes.Assets;
 using NanoEngine.ObjectTypes.Assets.Control;
 using NanoEngine.ObjectTypes.Control;
+using NanoEngine.StateManagement.StateMachine;
+using NanoEngine.StateManagement.States;
 
 namespace NanoEngine.Testing
 {
@@ -11,9 +15,32 @@ namespace NanoEngine.Testing
     {
         private string Direction;
 
+        public IStateMachine _StateMachine;
+
+        public int Timer;
+
         public TestMind()
         {
+            Timer = 0;
+            _StateMachine = new StateMachine();
+            _StateMachine.AddState(new State1());
+            _StateMachine.AddState(new State2());
+            _StateMachine.AddState(new State3());
+            _StateMachine.AddKeyboardTransition<State1, State2>(
+                KeyStates.Pressed, new List<Keys>() { Keys.NumPad1 }
+            );
+            _StateMachine.AddKeyboardTransition<State2, State3>(
+                KeyStates.Pressed, new List<Keys>() { Keys.NumPad2 }    
+            );
+            _StateMachine.AddMethodCheckTransition<State3, State1>(IsReadyToSwitch);
             Direction = "right";
+        }
+
+        public bool IsReadyToSwitch()
+        {
+            if (Timer > 360)
+                return true;
+            return false;
         }
 
         public void OnKeyDown(object sender, NanoKeyboardEventArgs args)
@@ -41,13 +68,7 @@ namespace NanoEngine.Testing
 
         public void OnKeyPressed(object sender, NanoKeyboardEventArgs args)
         {
-            if (args.MyKeys.Contains(Keys.Left))
-            {
-                Direction = "left";
-                Console.WriteLine("PRESSED LEFT");
-            }
-            if (args.MyKeys.Contains(Keys.Right))
-                Direction = "right";
+            _StateMachine.HandleKeyboardInput(this, args);
         }
 
         public void OnKeyReleased(object sender, NanoKeyboardEventArgs args)
@@ -62,7 +83,8 @@ namespace NanoEngine.Testing
 
         public override void Update()
         {
-
+            _StateMachine.Update<IAIComponent>(this);
+            Timer++;
         }
     }
 }

f08c5cb653d93fd4aa14ad746d1b9a3a88a57789
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2018-02-11 18:17:16 +0000

    test more and add an interface that tells the asset manager to pass itsself

--- NanoEngine/Testing/ZombieMind.cs
+++ NanoEngine/Testing/ZombieMind.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using NanoEngine.ObjectManagement.Interfaces;
 using NanoEngine.ObjectTypes.Assets;
 using NanoEngine.ObjectTypes.Assets.Control;
 using NanoEngine.StateManagement.StateMachine;
 
 namespace NanoEngine.Testing
 {
-    public class ZombieMind : AiComponent
+    public class ZombieMind : AiComponent, IAssetmanagerNeeded
     {
         private IStateMachine _stateMachine;
+
+        public IAssetManager AssetManager { get; set; }
 
         public override void Initialise()
         {
@@ -28,13 +31,18 @@ namespace NanoEngine.Testing
             );
 
             _stateMachine.AddState(new CheerState("cheer"), "cheer");
+            _stateMachine.AddState(new ChaseState(AssetManager.RetriveAsset("player")), "chase");
             _stateMachine.AddSuccessTransition("patrol", "cheer");
             _stateMachine.AddSuccessTransition("cheer", "patrol");
+            _stateMachine.AddMethodCheckTransition(CloseToPlayer, "patrol", "chase");
         }
 
         private bool CloseToPlayer()
         {
-
+            Vector2 playersPosition = AssetManager.RetriveAsset("player").Position;
+            if (Vector2.Distance(controledEntity.Position, playersPosition) < 100)
+                return true;
+            return false;
         }
 
         /// <summary>
@@ -43,7 +51,6 @@ namespace NanoEngine.Testing
         public override void Update()
         {
             _stateMachine.Update();
-            
         }
     }
 }

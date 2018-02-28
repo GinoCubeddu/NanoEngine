94d79b34d46efcdf2f82e0d86e5f9a14a97e2374
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-02-28 00:03:10 +0000

    Create a collision manager

---
+++ NanoEngine/Collision/CollisionResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NanoEngine.ObjectTypes.Assets;
+
+namespace NanoEngine.Collision
+{
+    public class CollisionResponse
+    {
+        public CollisionResponse(IAsset asset)
+        {
+            Asset = asset;
+        }
+
+        public IAsset Asset { get; }
+
+
+    }
+}

c2cfee7eb55637c1de3af7b57aeabbc6372bf435
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-02-28 00:03:10 +0000

    Create a collision manager

---
+++ NanoEngine/Collision/Manager/ICollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NanoEngine.ObjectTypes.Assets;
+
+namespace NanoEngine.Collision.Manager
+{
+    public interface ICollisionManager
+    {
+        /// <summary>
+        /// Checks to see if there is a collsion between an asset and a list
+        /// of assets
+        /// </summary>
+        /// <param name="asset">A tuple of an asset and its mind</param>
+        /// <param name="possibleCollisions">A list containingt tuples of assets and their minds</param>
+        void CheckCollision(
+            Tuple<IAsset, IAiComponent> asset,
+            IList<Tuple<IAsset, IAiComponent>> possibleCollisions
+        );
+    }
+}

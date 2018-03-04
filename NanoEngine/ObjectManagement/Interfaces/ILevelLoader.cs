c290298ed1709dc8958d7a91601e49ca5d560db1
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-03-04 14:24:29 +0000

    Makes a few changes to how tiles work
    Firstly the TileManager is now called LevelLoader
    and only has one public method while having 4 public static method.
    
    The Level loader's single LoadLevel method takes the following paramaters:
    	1) fileName
    	2) assets
    	3) aiComponents
    	4) assetFactory
    	5) aiFactory
    	6) uId
    
    This method will be called from the asset manager and all generated
    assets/minds will be created through the factories and added to the
    passed in dicts.
    
    The actual TileMap object has changed to now deserialise into the new
    TileMap object with its layers going into the new Layer object.
    
    Upon loading of the game within the game1 class the game coder will
    have to call the public static methods to add a Asset/Mind combo and
    a uName if wanted.

---
+++ NanoEngine/ObjectManagement/Interfaces/ILevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NanoEngine.ObjectTypes.Assets;
+
+namespace NanoEngine.ObjectManagement.Interfaces
+{
+    public interface ILevelLoader
+    {
+        /// <summary>
+        /// Loads the requested tile map assets and aiComponets into the passed in
+        /// dictonaries
+        /// </summary>
+        /// <param name="fileName">The name of the file</param>
+        /// <param name="assets">A dictonary for the assets</param>
+        /// <param name="aiComponents">A dictonary for the AiComonents</param>
+        /// <param name="assetFactory">A assetFactory instance</param>
+        /// <param name="aiFactory">A aiFactory instance</param>
+        /// <param name="uId">a uId counter for asset names that have not requested a specific uName</param>
+        void LoadTileMap(
+            string fileName, IDictionary<string, IAsset> assets,
+            IDictionary<string, IAiComponent> aiComponents, IAssetFactory assetFactory,
+            IAiFactory aiFactory, int uId
+        );
+    }
+}

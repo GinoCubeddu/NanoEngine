329269c6df5c41f4324189b4b737bbf6877eb0ce
GinoCubeddu <cubg1_14@uni.worc.ac.uk>
2018-03-06 21:27:00 +0000

    make AABB send side collision response object

--- NanoEngine/Collision/CollisionTypes/ICollisionType.cs
+++ NanoEngine/Collision/CollisionTypes/ICollisionType.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NanoEngine.Events.Args;
 using NanoEngine.ObjectTypes.Assets;
 
 namespace NanoEngine.Collision.CollisionTypes
@@ -15,6 +16,6 @@ namespace NanoEngine.Collision.CollisionTypes
         /// <param name="asset1">The first asset to be checked against</param>
         /// <param name="asset2">The second asset to be checked against</param>
         /// <returns>A boolean value telling us if there has been a collision</returns>
-        bool CheckCollision(IAsset asset1, IAsset asset2);
+        Tuple<NanoCollisionEventArgs, NanoCollisionEventArgs> CheckCollision(IAsset asset1, IAsset asset2);
     }
 }

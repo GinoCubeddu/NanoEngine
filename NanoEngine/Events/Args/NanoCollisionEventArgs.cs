f367de10032eb13842ab7ada80448559fa71e1f6
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-03-06 12:32:10 +0000

    Import collision side check from test project

---
+++ NanoEngine/Events/Args/NanoCollisionEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using NanoEngine.Collision;
+using NanoEngine.ObjectTypes.Assets;
+
+namespace NanoEngine.Events.Args
+{
+    class NanoCollisionEventArgs : EventArgs
+    {
+        // The asset that the object has collided with
+        public IAsset CollidedWith { get; set; }
+
+        // The side of the collision
+        public CollisionSide CollisionSide { get; set; }
+
+        // The amount of overlap that the objects have
+        public Vector2 CollisionOverlap { get; set; }
+    }
+}

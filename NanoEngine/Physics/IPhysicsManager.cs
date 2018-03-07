afd8a0e08241f3c53accaed9e1a36deef876193d
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-03-07 00:19:52 +0000

    Implement Josh's physics into the engine

---
+++ NanoEngine/Physics/IPhysicsManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NanoEngine.ObjectTypes.Assets;
+
+namespace NanoEngine.Physics
+{
+    internal interface IPhysicsManager
+    {
+        /// <summary>
+        /// Updates the physics on all the assets that are passed in
+        /// </summary>
+        /// <param name="assets">A list of assets to update phsyics on</param>
+        void UpdatePhysics(IList<IAsset> assets);
+    }
+}

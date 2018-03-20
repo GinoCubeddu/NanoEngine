96dc4d9035021569f5b41d1fc4189e5b031c8456
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-03-20 16:05:41 +0000

    Update the test assets to use the new service locator

--- NanoEngine/Testing/Assets/ZombieAsset.cs
+++ NanoEngine/Testing/Assets/ZombieAsset.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
 using NanoEngine.Collision.CollidableTypes;
+using NanoEngine.Core.Interfaces;
+using NanoEngine.Core.Locator;
 using NanoEngine.Core.Managers;
 using NanoEngine.ObjectTypes.Assets;
 
@@ -16,8 +18,8 @@ namespace NanoEngine.Testing.Assets
         /// </summary>
         public override void Initilise()
         {
-            SetTexture(ContentManagerLoad.Manager.LoadResource<Texture2D>("zombie"));
-            AssetAnimation = new Animation(this);
+            SetTexture(ServiceLocator.Instance.RetriveService<INanoContentManager>(DefaultNanoServices.ContentManager)
+                .LoadResource<Texture2D>("zombie")); AssetAnimation = new Animation(this);
             AssetAnimation.AddState("walkLeft", 50, 64, 0, 2, 4);
             AssetAnimation.AddState("walkRight", 50, 64, 1, 2, 4);
             AssetAnimation.AddState("cheer", 50, 64, 2, 2, 4);

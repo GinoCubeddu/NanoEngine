c3c059b70b771e272878e89b06ce07a0410126c3
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-03-26 12:29:58 +0100

    Add new method that just checks if 2 AABB are colliding but dont do any extra work

--- NanoEngine/Collision/CollisionTypes/IAABB.cs
+++ NanoEngine/Collision/CollisionTypes/IAABB.cs
@@ -8,6 +8,12 @@ namespace NanoEngine.Collision.CollisionTypes
 {
     public interface IAABB : ICollisionType
     {
-
+        /// <summary>
+        /// Checks to see if two axis aligned bounding boxes can collide
+        /// </summary>
+        /// <param name="asset1">The first asset to be checked against</param>
+        /// <param name="asset2">The second asset to be checked against</param>
+        /// <returns>A boolean value telling us if it is possible for a collision</returns>
+        bool CheckCanCollide(IAsset asset1, IAsset asset2);
     }
 }

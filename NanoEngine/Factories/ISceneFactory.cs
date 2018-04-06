ec68461950a5d2d86f3c9dc245a977e58f8a7e77
Gino Cubeddu <cubg1_14@uni.worc.ac.uk>
2018-04-06 14:06:51 +0100

    Offload creating screens to a factory from the scenemanager

---
+++ NanoEngine/Factories/ISceneFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NanoEngine.ObjectTypes.General;
+
+namespace NanoEngine.Factories
+{
+    public interface ISceneFactory
+    {
+        /// <summary>
+        /// Creates and initalises the requested game screen and returns it
+        /// </summary>
+        /// <typeparam name="T">The type of screen to create</typeparam>
+        /// <returns>The created game screen</returns>
+        IGameScreen CreateScreen<T>() where T : IGameScreen, new();
+    }
+}
